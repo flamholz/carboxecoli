74dadadc3d728bca5dfaaea15c82017329c44a70
Avi Flamholz <flamholz@gmail.com>
2020-06-19 17:28:36 -0700

    Rename a bunch of figure notebooks to accomodate eLife "figure supplements." Various edits.

--- notebooks/pltconfig.py
+++ notebooks/pltconfig.py
@@ -5,6 +5,7 @@ greys = sns.color_palette('Greys', n_colors=8)
 greens = sns.color_palette('Greens', n_colors=8)
 blue_greens = sns.color_palette("GnBu_d", n_colors=12)
 blues = sns.color_palette('Blues', n_colors=8)
+oranges = sns.color_palette('Oranges', desat=0.8)
 purples = sns.color_palette('Purples', n_colors=8)
 reddish_purple = sns.set_hls_values(sns.xkcd_rgb['reddish purple'], 0.9, 0.3, 1)
 reddish = sns.xkcd_rgb['raspberry']
